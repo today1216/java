cd9e6035d588ca8d4188d67f2bf5cd1549150970
today1216 <today1216@gmail.com>
2023-09-04 10:41:26 +0900

    2023-09-04_10:40

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 import java.util.StringTokenizer;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 class Main{
     public static void main(String[] args) throws IOException
