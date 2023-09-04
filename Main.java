a583ef7649a33f6ad2aead808098cdb11c6d2ce7
today1216 <today1216@gmail.com>
2023-09-04 10:37:11 +0900

    2023-09-04 10:37

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-
-import java.util.*;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 class Main{
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("test");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        System.out.println(a);
     }
 }
