c80a0e4a2ec9eeb7113998bef3941893e8ff317b
today1216 <today1216@gmail.com>
2023-09-04 10:51:10 +0900

    2023-09-04_10:51

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        list.add(Integer.parseInt(st.nextToken()));
+        System.out.println(list);
+    }
+}
