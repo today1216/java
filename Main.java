b1639a129903eb617d915e82814d391b47a1856f
today1216 <today1216@gmail.com>
2023-11-15 10:42:54 +0900

    2023-11-15_10:42_Ransomware

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 import java.util.*;
-import java.io.*;
 
 class Main{
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> list = new ArrayList<>();
-        list.add(Integer.parseInt(st.nextToken()));
-        System.out.println(list);
-        //test 2023-11-14_15:27(2)
+        System.out.println("hello");
     }
 }
