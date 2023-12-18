dbbd9872a69c01c444ea5166870ed8ad024cc40f
today1216 <today1216@gmail.com>
2023-12-18 11:19:52 +0900

    2023_12_18_Ransomware_java

--- Main.java
+++ Main.java
@@ -1,8 +1,35 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main{
-    public static void main(String[] args)
-    {
-        System.out.println("hello");
+public class Main {
+    public static void main(String[] args) {
+        String ipAddress = "8.8.8.8";
+
+        try {
+            // Build the command
+            ProcessBuilder processBuilder = new ProcessBuilder("ping", ipAddress);
+            
+            // Start the process
+            Process process = processBuilder.start();
+            
+            // Read the output of the command
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            
+            // Print the output
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+            
+            // Print the exit code
+            System.out.println("Exit Code: " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
